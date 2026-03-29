3ea42d8b6dc9d0f78e6bb21c8e980e2ad70c20d0
Alessio Cantina <alessio.cantina@gmail.com>
2026-03-29 20:27:39 +0200

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,6 @@ const test = require('node:test');
 const assert = require('node:assert');
 const { hello } = require('../src/index');
 
-test('hello() returns Hello World', () => {
+test('hello() returns Hello Lab', () => {
   assert.strictEqual(hello(), 'Hello World');
 });
